0295c02214c514e2738e5cd3549f9b7cfef04f71
judytdu <jtdu@alumni.princeton.edu>
2024-02-26 21:55:53 -0500

    Added baselines.py and test_baselines.py.

--- supervised_baselines/baselines.py
+++ supervised_baselines/baselines.py
@@ -4,17 +4,23 @@ from sklearn import datasets
 import supervised_baselines.define_models as define_models
 import supervised_baselines.optuna_config as optuna_config
 
+from neptune import management
+import neptune.new as neptune
+import neptune.new.integrations.optuna as npt_utils
+
 """Main script. Creates class capable of training, monitoring, and logging
 results from supervised learning. By default, performs hyperparameter tuning
-on folds of the input training data and reports performance metrics on 
+on folds of the input training data, and reports performance metrics on 
 NeptuneAI. 
     
 Typical Usage Examples:
     example_model = SupervisedBaselines(
-        model_type="all_regressors", 
+        model_type="all_regressors, 
         log_file="supervised_baseline.log",
         optuna_config_dict=optuna_config.config_dict,
-    )
+        neptune_project_name="project_name", 
+        neptune_workspace="neptuneai_username", 
+        neptune_api_token="api_token")
     example_model.fit(X, y) 
 """
 
@@ -25,6 +31,13 @@ class SupervisedBaselines():
         model_type (str): Name of model. For valid options, run 
             define_models.get_model_names(all_regressors=True) or 
             define_models.get_model_names(all_regressors=False).
+        neptune_project_name (str): Name of NeptuneAI project.
+        neptune_workspace (str): Name of NeptuneAI workspace. Often a username.
+        neptune_api_token (str, optional): Neptune API tokens can be passed 
+            directly or set as an environment variable. If neptune_api_token 
+            is None and NEPTUNE_API_TOKEN is not an environment variable, no 
+            results are logged to NeptuneAI. Defaults to None. 
+            For more info, see https://app.neptune.ai/get_my_api_token.
         log_file (str, optional): File name or path to write logging statements. 
             Defaults to "supervised_baseline.log".
         optuna_config (dict, optional):  Optuna configuration file. 
@@ -32,6 +45,8 @@ class SupervisedBaselines():
     """
     def __init__(
             self, model_type: str, 
+            neptune_project_name: str, neptune_workspace: str, 
+            neptune_api_token: str=None,
             log_file: str="supervised_baseline.log",
             optuna_config_dict: dict=optuna_config.config_dict,
             ):
@@ -42,6 +57,8 @@ class SupervisedBaselines():
         self._configure_logger(log_file)
         self._configure_model_and_hyperparams(model_type)
         self._configure_optuna_hyperparams(optuna_config_dict)
+        self._configure_neptuneai(
+            neptune_project_name, neptune_workspace, neptune_api_token)
     
     def load_data(self):
         """Defines self.X, self.y, and self.split_groups
@@ -88,6 +105,92 @@ class SupervisedBaselines():
         else:
             self.global_tuning_params, self.model_hyperparams = None, None        
         
+        
+    def _configure_neptuneai(self, neptune_project_name, neptune_workspace, 
+                                  neptune_api_token):
+        logging.info(
+            f"Configuring NeptuneAI: {neptune_workspace}/{neptune_project_name}"
+        )
+        # Optionally load token from environment
+        self.neptune_project = f"{neptune_workspace}/{neptune_project_name}"
+        self.neptune_logger, self.neptune_callback = None, None        
+        if neptune_api_token is None:
+            if "NEPTUNE_API_TOKEN" in os.environ:
+                neptune_api_token = os.environ["NEPTUNE_API_TOKEN"]
+            else:
+                logging.info("""neptune_api_token not found. 
+                             For more info, see neptune.ai/get_my_api_token.""")
+        self.__neptune_api_token = neptune_api_token
+        
+        # Create project if it does not yet exist
+        if self.__neptune_api_token is not None:
+            neptune_projects = management.get_project_list(
+                api_token=self.__neptune_api_token
+            )
+            if self.neptune_project not in neptune_projects:
+                management.create_project(
+                    workspace=neptune_workspace,
+                    name=neptune_project_name,
+                    visibility='workspace',
+                    api_token=self.__neptune_api_token
+                )            
+    
+    def neptune_initialize_run(self, model_name: str):
+        self.neptune_logger = neptune.init_run(
+            project=self.neptune_project,
+            api_token=self.__neptune_api_token,
+            name=model_name,
+            #source_files=["*.py", "requirements.txt"],
+            capture_stdout=True,
+            capture_stderr=True,
+            capture_hardware_metrics=True
+        )
+        self.neptune_callback = npt_utils.NeptuneCallback(self.neptune_logger) 
+        
+    def neptune_add_token(self, neptune_api_token: str):
+        self.__neptune_api_token = neptune_api_token
+    
+    def neptune_del_token(self):
+        del self.__neptune_api_token
+    
+    def neptune_log_metadata(self, model_name: str):
+        for k, v in self.global_tuning_params.items():
+            self.neptune_logger[k] = v
+        for k, v in self.model_hyperparams[model_name].items():
+            self.neptune_logger[k] = v
+            
+    def fit(self):
+        for mt in self.model_types:
+            if self.__neptune_api_token is not None:
+                self.neptune_initialize_run(model_name=mt)
+                self.neptune_log_metadata(model_name=mt)
+            
+            if self.global_tuning_params is not None:
+                # Perform optuna hyperparameter tuning
+                self.optuna[mt], self.model[mt] = run_optuna.optimize_hyperparams(
+                    # Input Data
+                    self.X, self.y,
+                    # KFold Grouping
+                    split_groups = split_groups,
+                    # Sampling, Train/Eval Specifications
+                    hyperparam_config = self.model_hyperparams[mt],
+                    define_model_fxn = define_models.define_models,
+                    define_model_params = {"model_name": mt},
+                    train_model_fxn = lambda model, X, y: model.fit(X,y), 
+                    evaluate_model_fxn = lambda model, X, y: model.score(X,y),
+                    # Neptune Logger
+                    neptune_callback = [self.neptune_callback],
+                    # Trial and Fold Specifications: 
+                        # n_trials, n_splits, train_size, 
+                        # eval_params, optimization_direction
+                    **self.global_tuning_params
+                )
+                self.optuna_best_performance[mt] = self.optuna[mt].best_value
+            if self.__neptune_api_token is not None:
+                self.neptune_logger.stop()
+                self.neptune_callback = None
+        self.neptune_del_token()
+        
 if __name__ == '__main__':
     # Run typical usage example
     class IrisClassifier(SupervisedBaselines):
@@ -103,8 +206,9 @@ if __name__ == '__main__':
             super().fit(self.X, self.y)
     
     iris_model = IrisClassifier(
-        model_type="all_classifiers"
-    )
+        model_type="all_classifiers",
+        neptune_project_name ="supervised-baselines-example", 
+        neptune_workspace="drjudydu")
     iris_model.load_data()
     iris_model.fit()
     print(self.optuna_best_performance)
--- tests/test_baselines.py
+++ tests/test_baselines.py
@@ -1,8 +1,16 @@
 import os
 from supervised_baselines import baselines
 
-reg = baselines.SupervisedBaselines("all_regressors")
-clf = baselines.SupervisedBaselines("all_classifiers")
+# Initialize regressor and classifier classes
+os.system("source ~/neptune_api_token.sh")
+params = {
+    model_type = "all_regressors",
+    neptune_project_name ="supervised-baselines-example", 
+    neptune_workspace="drjudydu"
+}
+reg = baselines.SupervisedBaselines(**params)
+params["model_type"] = "all_classifiers"
+clf = baselines.SupervisedBaselines(**params)
 
 # Test __init__() and related helper functions
 def test_repr(regressors=reg, classifiers=clf):
@@ -24,6 +32,25 @@ def test_configure_model_classifiers(classifiers=clf):
 def test_configure_optuna_hyperparams(classifiers=clf):
     assert len(classifiers.model_hyperparams) == 15
 
+# Test Neptune
+def test_configure_neptuneai(classifiers=clf):
+    assert len(classifiers.__neptune_api_token) > 0
+    
+def test_neptune_del_token(classifiers=clf):
+    classifiers.neptune_del_token()
+    assert len(classifiers.__neptune_api_token) == 0
+    
+def test_neptune_add_token(classifiers=clf):
+    classifiers.neptune_add_token(os.environ["NEPTUNE_API_TOKEN"])
+    assert len(classifiers.__neptune_api_token) > 0
+    
+def test_neptune_initialize_run(classifiers=clf):
+    classifiers.neptune_initialize_run("test_baselines")
+    assert True
+
+# Test Fit
+
+# Test Eval
 
 # Break down test objects
-del reg, clf
+del params, reg, clf
